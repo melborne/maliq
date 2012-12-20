096abe1e4d5311dd62170d78dfbe3550aa09f9b8
kyoendo <postagie@gmail.com>
2012-12-20 15:10:22 +0900

    Move Maliq::Builder to a file

---
+++ lib/maliq/builder.rb
@@ -0,0 +1,49 @@
+class Maliq::Builder
+  def initialize(files, opts)
+    @opts = opts
+    @csses = opts.delete(:css)
+    @mdfiles = get_markdown_files(files)
+  end
+
+  def get_markdown_files(argv)
+    argv.select { |f| f.match /\.(md|markdown)$/ }
+        .tap { |res|
+          if res.empty?
+            abort "Must pass one or more markdown filenames to build xhtml output."
+          end
+        }
+  end
+  private :get_markdown_files
+
+  def run!
+    lastname = nil
+    @mdfiles.each_with_index do |fname, fno|
+      if @opts[:seq] && lastname
+        seq_name = Maliq::FileUtils.create_filename(lastname)
+      end
+
+      # Split a file at split markers to several chapters,
+      # each of which has title a label.
+      chapters = Maliq::FileUtils.split(fname)
+
+      chapters.each do |title, text|
+        #this change filenames with sequential names.
+        if @opts[:seq] && !fno.zero?
+          title = seq_name.tap { |s| seq_name = seq_name.next }
+        end
+
+        dest = title.basename_with(:xhtml)
+        convert_and_save(text, dest)
+        lastname = title
+      end
+    end
+  end
+
+  private
+  def convert_and_save(md, dest)
+    conv = Maliq::Converter.new(md, css:@csses)
+    conv.set_meta(liquid:@opts[:liquid]) if @opts[:liquid]
+    conv.save(dest)
+    puts "'#{dest}' created."
+  end
+end
