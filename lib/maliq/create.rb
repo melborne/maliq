f8318ea7bcb5e61ac03a3301de01f208d03dac6d
kyoendo <postagie@gmail.com>
2012-12-21 10:31:43 +0900

    Refactoring #run!

--- lib/maliq/create.rb
+++ lib/maliq/create.rb
@@ -1,10 +1,10 @@
 # Maliq::Create receive markdown filename(s)
 # and create xhtml file(s).
-
 class Maliq::Create
   def initialize(files, opts={})
     @opts = opts
     @csses = opts.delete(:css)
+    @nav = opts.delete(:nav) || opts.delete(:toc)
     @mdfiles = get_markdown_files(files)
   end
 
@@ -20,25 +20,25 @@ class Maliq::Create
 
   def run!
     lastname = nil
-    @mdfiles.each_with_index do |fname, fno|
-      dirname = File.dirname(fname)
+    @mdfiles.each_with_index do |filename, fno|
+      dirname = File.dirname(filename)
       if @opts[:seq] && lastname
-        seq_name = Maliq::FileUtils.create_filename(lastname)
+        seq_fname = Maliq::FileUtils.create_filename(lastname)
       end
 
       # Split a file at split markers to several chapters,
-      # each of which has title a label.
-      chapters = Maliq::FileUtils.split(fname)
+      # each of which become a file.
+      chapters = Maliq::FileUtils.split(filename)
 
-      chapters.each do |title, text|
+      chapters.each do |fname, content|
         #this change filenames with sequential names.
         if @opts[:seq] && !fno.zero?
-          title = seq_name.tap { |s| seq_name = seq_name.next }
+          fname = seq_fname.tap { |s| seq_fname = seq_fname.next }
         end
 
-        dest = title.basename_with(:xhtml)
-        convert_and_save(text, File.join(dirname, dest))
-        lastname = title
+        dest = File.join(dirname, fname.basename_with(:xhtml))
+        convert_and_save(content, dest)
+        lastname = fname
       end
     end
   end
